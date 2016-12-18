8a6fde34e12beecddd599881cedb1a4af4ef5231
vovochka2016 <extremevovan@gmail.com>
2016-12-18 18:02:11 +0200

    Update drugs.js

--- client/controllers/drugs.js
+++ client/controllers/drugs.js
@@ -1,38 +1,38 @@
 var myApp = angular.module('myApp');
 
-myApp.controller('BooksController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
-	console.log('BooksController loaded...');
+myApp.controller('drugsController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
+	console.log('drugsController loaded...');
 
-	$scope.getBooks = function(){
-		$http.get('/api/books').success(function(response){
-			$scope.books = response;
+	$scope.getdrugs = function(){
+		$http.get('/api/drugs').success(function(response){
+			$scope.drugs = response;
 		});
 	}
 
-	$scope.getBook = function(){
+	$scope.getdrug = function(){
 		var id = $routeParams.id;
-		$http.get('/api/books/'+id).success(function(response){
-			$scope.book = response;
+		$http.get('/api/drugs/'+id).success(function(response){
+			$scope.drug = response;
 		});
 	}
 
-	$scope.addBook = function(){
-		console.log($scope.book);
-		$http.post('/api/books/', $scope.book).success(function(response){
-			window.location.href='#/books';
+	$scope.adddrug = function(){
+		console.log($scope.drug);
+		$http.post('/api/drugs/', $scope.drug).success(function(response){
+			window.location.href='#/drugs';
 		});
 	}
 
-	$scope.updateBook = function(){
+	$scope.updatedrug = function(){
 		var id = $routeParams.id;
-		$http.put('/api/books/'+id, $scope.book).success(function(response){
-			window.location.href='#/books';
+		$http.put('/api/drugs/'+id, $scope.drug).success(function(response){
+			window.location.href='#/drugs';
 		});
 	}
 
-	$scope.removeBook = function(id){
-		$http.delete('/api/books/'+id).success(function(response){
-			window.location.href='#/books';
+	$scope.removedrug = function(id){
+		$http.delete('/api/drugs/'+id).success(function(response){
+			window.location.href='#/drugs';
 		});
 	}
 }]);
